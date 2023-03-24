134e59031f500592c627302cf583b727ba3317a6
M Dhiya Ulhaq <uhqdev@192.168.1.16>
2023-03-24 14:35:04 +0800

    fix styling

--- src/components/moleculas/table.jsx
+++ src/components/moleculas/table.jsx
@@ -1,34 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import SpinnerLoading from '../atoms/SpinnerLoading';
 
 export default function Table(props) {
   const {
     data,
+    isLoading,
   } = props;
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" className="px-6 py-3">Id</th>
-            <th scope="col" className="px-6 py-3">Avatar</th>
-            <th scope="col" className="px-6 py-3">Name</th>
-            <th scope="col" className="px-6 py-3">Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data?.map((user) => (
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={user.id}>
-              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{user.id}</th>
-              <td className="px-6 py-4">
-                <img src={user.avatar} alt={user.avatar} width="100" height="100" className="rounded-full w-10" />
-              </td>
-              <td className="px-6 py-4">{`${user.first_name} ${user.last_name}`}</td>
-              <td className="px-6 py-4">{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {
+        isLoading ? (
+          <SpinnerLoading />
+        ) : (
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-6 py-3">Id</th>
+                <th scope="col" className="px-6 py-3">Avatar</th>
+                <th scope="col" className="px-6 py-3">Name</th>
+                <th scope="col" className="px-6 py-3">Email</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.map((user) => (
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={user.id}>
+                  <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{user.id}</th>
+                  <td className="px-6 py-4">
+                    <img src={user.avatar} alt={user.avatar} width="100" height="100" className="rounded-full w-10" />
+                  </td>
+                  <td className="px-6 py-4">{`${user.first_name} ${user.last_name}`}</td>
+                  <td className="px-6 py-4">{user.email}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )
+      }
     </div>
   );
 }
@@ -40,6 +48,10 @@ const usersData = {
   last_name: PropTypes.string,
 };
 
+Table.defaultProps = {
+  isLoading: false,
+};
 Table.propTypes = {
   data: PropTypes.arrayOf(PropTypes.shape(usersData)).isRequired,
+  isLoading: PropTypes.bool,
 };
